e09db791a2bc315e47d389e7490714c140c0ba86
Andre van de Ven <andrebvandeven@gmail.com>
2023-12-01 17:27:54 -0500

    models created

--- backend/Studious/models.py
+++ backend/Studious/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school = models.CharField(max_length=100)
+    major = models.CharField(max_length=100)
+    year = models.CharField(max_length=100)
+    # Additional fields if needed, other than first_name, last_name, email
+
+
+class StudySession(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    class_name = models.CharField(max_length=100)
+    description = models.TextField()
+    attendees = models.ManyToManyField(User, related_name="signed_up_sessions")
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    UserProfile.objects.get_or_create(user=instance)
+    instance.userprofile.save()
